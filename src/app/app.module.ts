cda61d917a4948219c62640f698a45437c760cb5
AndrewJBateman <gomezbateman@yahoo.com>
2022-02-28 18:26:51 +0100

    chore: updated to Angular v13

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,16 +13,15 @@ import { File } from '@ionic-native/file/ngx';
 import { FileOpener } from '@ionic-native/file-opener/ngx';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    File,
-    FileOpener
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        File,
+        FileOpener
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
